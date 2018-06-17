6f51573354d6bf3eb27de1100465e7a20153d6be
Abay Bektursun <abaybektursun@gmail.com>
2018-06-17 17:21:57 -0500

    batch norm scatter plot

--- visual.js
+++ visual.js
@@ -1,5 +1,9 @@
 import * as d3 from "d3";
 import * as c3 from "c3";
+import Chart from "chart.js";
+
+import * as utils from "./utils";
+var stats = require("stats-lite")
 
 
 // Loss
@@ -32,3 +36,120 @@ module.exports = {
   chart_losses_0: chart_losses_0,
   chart_accuracy_0: chart_accuracy_0,
 };
+
+
+/*--------------------------- Scatter plot --------------------------*/
+/*-------------------------------------------------------------------*/
+
+var color = Chart.helpers.color;
+
+var numPoints = 100
+var dataPoints = []
+var i;
+for(i = 0; i < numPoints; i++){
+  dataPoints.push({
+    x: randomScalingFactorX(),
+    y: randomScalingFactorY(),
+  })
+};
+
+
+var scatterChartData = {
+	datasets: [{
+		label: 'Data Points',
+		borderColor: window.chartColors.red,
+		backgroundColor: color(window.chartColors.red).alpha(0.2).rgbString(),
+		data: dataPoints
+	}]
+};
+
+window.onload = function() {
+	var ctx = document.getElementById('canvas').getContext('2d');
+	window.myScatter = Chart.Scatter(ctx, {
+		data: scatterChartData,
+		options: {
+      scales: {
+        yAxes: [{
+            ticks: {
+                beginAtZero:true
+            }
+        }],
+        xAxes: [{
+            ticks: {
+                beginAtZero:true
+            }
+        }]
+      },
+			title: {
+				display: true,
+				text: 'Random Data'
+			},
+		}
+	});
+};
+
+// Radmonize the Data
+document.getElementById('randomizeData').addEventListener('click', function() {
+
+  // Radmonize the Data
+		scatterChartData.datasets[0].data = scatterChartData.datasets[0].data.map(function() {
+			return {
+				x: randomScalingFactorX(),
+				y: randomScalingFactorY()
+			};
+		});
+
+  window.myScatter.options.title.text = 'Random Data';
+  //window.myScatter.options.gridLines.lineWidth = 2;
+	window.myScatter.update();
+});
+
+// Subtract Mean
+document.getElementById('subMean').addEventListener('click', function() {
+  var xs = [];
+  var ys = [];
+  scatterChartData.datasets[0].data.forEach(function(point) {
+    xs.push(point.x);
+    ys.push(point.y);
+  });
+
+  var xmean = stats.mean(xs);
+  var ymean = stats.mean(ys);
+
+  // Radmonize the Data
+	scatterChartData.datasets[0].data = scatterChartData.datasets[0].data.map(point =>{
+    return{
+	    x: point.x - xmean,
+	    y: point.y - ymean
+		};
+  });
+
+  window.myScatter.options.title.text = 'Centered Around Zero';
+	window.myScatter.update();
+});
+
+// Scale The Variance
+document.getElementById('normVar').addEventListener('click', function() {
+  var xs = [];
+  var ys = [];
+  scatterChartData.datasets[0].data.forEach(function(point) {
+    xs.push(point.x);
+    ys.push(point.y);
+  });
+
+  var xmean = stats.mean(xs);
+  var ymean = stats.mean(ys);
+
+  var xstd = stats.stdev(xs);
+  var ystd = stats.stdev(ys);
+
+  // Radmonize the Data
+	scatterChartData.datasets[0].data = scatterChartData.datasets[0].data.map(point =>{
+    return{
+	    x: (point.x - xmean)/xstd,
+	    y: (point.y - ymean)/ystd
+		};
+  });
+  window.myScatter.options.title.text = 'Normalized';
+	window.myScatter.update();
+});
